c5770c87d40b28debd654abfa01484a3913bda72
abinash <abinashrai14@gmail.com>
2024-10-23 18:50:29 +0545

    change

--- metronic-tailwind-next/src/pages/index.tsx
+++ metronic-tailwind-next/src/pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="w-full">
+        <label className="form-label mb-2" htmlFor="range_1">
+          Example range
+        </label>
+        <input className="range" id="range_1" max="10" min="0" type="range" />
+      </div>
+    </>
+  );
 }
