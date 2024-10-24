8adf26bf418ff8473dfce123ffeb6a0af515622d
abinash <abinashrai14@gmail.com>
2024-10-24 09:20:08 +0545

    add metronic

---
+++ metronic-tailwind-next/src/metronic/core/components/sticky/types.ts
@@ -0,0 +1,18 @@
+export interface KTStickyConfigInterface {
+	target: string,
+	name: string,
+	class: string,
+	zindex: string,
+	top: string,
+	left: string,
+	right: string,
+	width: string | number | object,
+	offset: number,
+	reverse: boolean,
+	release: string,
+	activate: string
+}
+
+export interface KTStickyInterface {		
+	update(): void;
+}
